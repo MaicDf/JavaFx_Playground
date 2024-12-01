15c165a39572a7a618855ccc7667a2e148665cad
Maik PCgamer <mdfontechad@unal.edu.co>
2024-12-01 14:07:34 -0500

    first

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/e/a0011cd232ab001f102ca28755af8853
@@ -0,0 +1,6 @@
+module javaFxNotes {
+	requires javafx.controls;
+	requires javafx.graphics;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
---
+++ Playground/src/prueba/holaMundo.java
@@ -0,0 +1,14 @@
+package prueba;
+
+public class holaMundo {
+
+	public holaMundo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		 System.out.println("Hola mundo");
+
+	}
+
+}
